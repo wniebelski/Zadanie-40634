9d1d1d17fd36ce51f97e1818c9838ba69aeccfc2
wniebelski <wniebelski@gmail.com>
2022-09-27 09:05:54 +0200

    rozwiazanie

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function liczenie (n){
+    if (n >= 100 && n <= 200){
+        console.log('Liczba znajduje się w przedziale')
+    }else {
+        console.log('Liczba nie znajduje się w przedziale')
+    }
+}
+liczenie(150)
